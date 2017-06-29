240fc75a9c5371532eb6403d22e4bad82fbb2dad
KEVIN R BURNHAM <kb@KEVINs-MacBook-Pro.local>
2017-06-29 08:05:44 -0500

    updated to work with new faster search functions

--- bd_lyric_search/app.R
+++ bd_lyric_search/app.R
@@ -36,7 +36,9 @@ server <- function(input, output) {
 
     #output$lyrics <- renderText("Hello")
     output$lyrics <- renderUI({
+      if (input$search_text == "") return("")
       matches <- find_bd_lyrics(input$search_text)
+      if (nrow(matches) == 0) return("No matches found.")
       line_matches <- find_line_matches(matches$Song[1], input$search_text)
       highlight_matches(song_name = matches$Song[1], line_matches) %>% HTML()
       })
